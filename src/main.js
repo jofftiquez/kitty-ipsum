7e4df3ada5c2ffa91a52ed7d0189a11f3b175a8b
Joff Tiquez <jofftiquez@gmail.com>
2018-12-18 17:59:19 +0800

    added vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import './firebase';
 import './vuetify';
 import './mixins';
 import app from './app';
 import router from './router';
+import store from './vuex';
 import VueClipboard from 'vue-clipboard2';
 import SocialSharing from 'vue-social-sharing';
 
@@ -17,6 +19,7 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   router,
+  store,
   components: { app },
   template: '<app/>'
 });
