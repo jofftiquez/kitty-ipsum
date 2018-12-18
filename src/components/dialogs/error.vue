28e8a7cf29bb2394f1d6ae7c7ef921abef08bb4e
Joff Tiquez <jofftiquez@gmail.com>
2018-12-18 12:52:54 +0800

    New error dialog

---
+++ src/components/dialogs/error.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  v-dialog(v-model="dialog2" width="400")
+    v-card(style="border-radius: 10px;")
+      v-card-text.text-xs-center
+        h1 Oops!
+        br
+        p {{errorMessage}}
+        v-btn(large round color="primary" @click="dialog2 = false") 
+          strong Meow!
+    
+</template>
+
+<script>
+  export default {
+    props: {
+      dialog: Boolean,
+      errorMessage: ''
+    },
+    data() {
+      return {
+        dialog2: this.dialog,
+      }
+    },
+    watch: {
+      dialog(val) {
+        this.dialog2 = val;
+      },
+      dialog2(val) {
+        if (!val) {
+          this.$emit('close', false);
+        } 
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  h1 {
+    color: #2c3e50 !important;
+  }
+</style>
